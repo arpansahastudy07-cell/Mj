3a360c4ebcda2bf2013c3e0b1de8d74725139e3c
AS <arpansaha.study07@gmail.com>
2026-09-10 09:54:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,16 +9,16 @@ const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-72.png',
-  './icons/icon-96.png',
-  './icons/icon-128.png',
-  './icons/icon-144.png',
-  './icons/icon-152.png',
-  './icons/icon-192.png',
-  './icons/icon-384.png',
-  './icons/icon-512.png',
-  './icons/maskable-192.png',
-  './icons/maskable-512.png'
+  './icon-72.png',
+  './icon-96.png',
+  './icon-128.png',
+  './icon-144.png',
+  './icon-152.png',
+  './icon-192.png',
+  './icon-384.png',
+  './icon-512.png',
+  './maskable-192.png',
+  './maskable-512.png'
 ];
 
 // Third-party assets the page loads (font CSS, ExcelJS). Cached opportunistically
